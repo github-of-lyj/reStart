7c01c7634693deb4182483332b6a23e80fe0c49d
Alex <Alex@lyj.com>
2023-05-31 23:02:05 +0800

    2023.5.31

--- src/main/java/TwoPrice/Two_Sum_II_Input_Array_Is_Sorted.java
+++ src/main/java/TwoPrice/Two_Sum_II_Input_Array_Is_Sorted.java
@@ -1,2 +1,18 @@
-package TwoPrice;public class Two_Sum_II_Input_Array_Is_Sorted {
+package TwoPrice;
+
+public class Two_Sum_II_Input_Array_Is_Sorted {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length-1;
+        while (left != right){
+            if (numbers[left] + numbers[right] == target)
+                break;
+            if (numbers[left] + numbers[right] < target)
+                left++;
+            if (numbers[left] + numbers[right] > target)
+                right--;
+        }
+        int[] result = {left+1,right+1};
+        return result;
+    }
 }
