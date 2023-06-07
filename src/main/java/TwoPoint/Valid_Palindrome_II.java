3c6ee50992a3e9c32259ce6fad72671c066ebe8a
Alex <Alex@lyj.com>
2023-06-07 11:10:23 +0800

    2023.6.7

--- src/main/java/TwoPoint/Valid_Palindrome_II.java
+++ src/main/java/TwoPoint/Valid_Palindrome_II.java
@@ -1,6 +1,15 @@
 package TwoPoint;
 
 public class Valid_Palindrome_II {
+    /*
+     * @author 林英杰
+     * @since JDK 版本 1.8++
+     * @Method validPalindrome(String s):删除前判断是否为回文字符串  isPalindrome(String s)：删除后判断是否是回文字符串
+     * @param
+     * @return
+     * @Address
+     * @TitleName
+     */
     public static boolean isPalindrome(String s){
         int left = 0;
         int right = s.length() - 1;
@@ -15,17 +24,18 @@ public class Valid_Palindrome_II {
     }
 
     public static boolean validPalindrome(String s) {
+        //双指针
         int left = 0;
         int right = s.length() - 1;
-        boolean mark = false;
         while(left <= right){
+            //相等情况 左指针++ 右指针--
             if(s.charAt(left) == s.charAt(right)){
                 left++;
                 right--;
             }else{
+                //不相等情况 剔除疑似障碍元素：考虑到障碍元素下标可能为 left+1 或为 right-1，因此都需要判断，若有一方为 True，则最终结果为 True
                 return isPalindrome(s.substring(left, right)) || isPalindrome(s.substring(left + 1, right + 1));
             }
-
         }
         return true;
     }
@@ -34,3 +44,4 @@ public class Valid_Palindrome_II {
         System.out.println(validPalindrome("ebcbbececabbacecbbcbe"));
     }
 }
+
