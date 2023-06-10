8fafc3f0d79a643c415478e3f4987355e13113c0
Alex <Alex@lyj.com>
2023-06-10 09:38:46 +0800

    2023.6.10 twice

--- src/main/java/Binary/Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
+++ src/main/java/Binary/Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
@@ -1,2 +1,53 @@
-package Binary;public class Find_First_and_Last_Position_of_Element_in_Sorted_Array {
+package Binary;
+
+public class Find_First_and_Last_Position_of_Element_in_Sorted_Array {
+    /*
+     * @author 林英杰
+     * @since JDK 版本 1.8++
+     * @Method searchRange 寻找值为 target 的开始位置和终止位置
+     * @param int[] nums 目标数组 int target：目标数据
+     * @return result：结果数组
+     * @Address https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/
+     * @TitleName 34. 在排序数组中查找元素的第一个和最后一个位置
+     */
+
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = {-1,-1};
+        if (nums.length == 0)
+            return result;
+        int begin = -1;
+        int left = 0,right = nums.length-1;
+        int mid;
+        while (left <= right){
+            mid = (left + right)/2;
+            if (nums[mid] == target) {
+                begin = mid;
+                break;
+            }else {
+                if (nums[mid] > target)
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+        }
+        if (begin != -1){
+            result[0] = begin;
+            result[1] = begin;
+            int temp = begin + 1;
+            while (temp <= nums.length - 1){
+                if (nums[temp] == target){
+                    result[1] = temp;
+                }
+                temp++;
+            }
+            temp = begin - 1;
+            while(temp >= 0){
+                if (nums[temp] == target){
+                    result[0] = temp;
+                }
+                temp--;
+            }
+        }
+        return result;
+    }
 }
